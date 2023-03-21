df96cb601bb1e6a8769483ad4995b582e142b75a
madara1319 <mateusz.bik@op.pl>
2023-03-21 21:12:36 +0100

    Starting with the slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import "../scss/main.scss";
 import "../js/burger-menu.js";
 import "../js/sticky-menu.js";
+import "../js/offer--slider";
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
 
+new Glide('.glide').mount({ Controls, Breakpoints });
 
 
 // uncomment the lines below to enable PWA
---
+++ src/js/offer--slider.js
@@ -0,0 +1,33 @@
+// const slider = new Slider(".offer--slider",{
+//     slidesPerView: 4,
+//     spaceBetween: 50,
+//     loop: true,
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slideActiveClass: "active",
+//     navigation: {
+//       nextEl: ".next",
+//       prevEl: ".prev"
+//     },
+//     pagination: {
+//       el: ".pagination",
+//       clickable: true
+//     },
+//     autoplay: {
+//       enabled: true,
+//       delay: 5000
+//     },
+//     // Media
+//     // breakpoints: {
+//     //   // when window width is >= 320px
+//     //   320: {
+//     //     slidesPerView: 2,
+//     //     spaceBetween: 20
+//     //   },
+//     //   // when window width is >= 480px
+//     //   480: {
+//     //     slidesPerView: 4,
+//     //     spaceBetween: 30
+//     //   },
+//     // }
+//   });
