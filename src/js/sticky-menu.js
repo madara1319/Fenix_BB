cb1e19d4229969ea3d11644466e8c98e54f777a1
madara1319 <mateusz.bik@op.pl>
2023-07-04 20:58:11 +0200

    Changed few things

--- src/js/sticky-menu.js
+++ src/js/sticky-menu.js
@@ -1,5 +1,6 @@
 window.onscroll = function () {
   stickyHeader();
+  console.log(window.pageYOffset);
 };
 
 var header = document.querySelector(".head__bar--js");
@@ -8,8 +9,19 @@ var sticky = header.offsetTop;
 console.log(header);
 
 
+
+// function stickyHeader() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("menu--sticky");
+//     header.classList.remove("head__bar");
+//   } else {
+//     header.classList.remove("menu--sticky");
+//     header.classList.add("head__bar");
+//   }
+// }
+
 function stickyHeader() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > 0) {
     header.classList.add("menu--sticky");
     header.classList.remove("head__bar");
   } else {
